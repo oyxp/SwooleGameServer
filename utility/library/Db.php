89f29d86777190c5d06d914606ea4bd845c67cb9
SnailZED <584119837@qq.com>
2019-07-09 16:42:10 +0800

    db pool

--- utility/library/Db.php
+++ utility/library/Db.php
@@ -37,15 +37,59 @@ class Db extends AbstractPool
     public function __call($name, $arguments)
     {
         /** @var Medoo $object */
+        return $this->callDbMethod($name, $arguments);
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Throwable
+     */
+    private function callDbMethod($name, $arguments)
+    {
         $object = $this->pop();
         try {
             $ret = call_user_func_array([$object, $name], $arguments);
             $this->recycle($object);
             return $ret;
         } catch (\Throwable $throwable) {
+            if ($this->isBreak($throwable)) {
+                unset($object);
+                $this->create();
+                return $this->callDbMethod($name, $arguments);
+            }
             $this->recycle($object);
             throw $throwable;
         }
+    }
+
+    /**判断数据库是否断线
+     * @param \Throwable $e
+     * @return bool
+     */
+    protected function isBreak($e)
+    {
+        $info = [
+            'server has gone away',
+            'no connection to the server',
+            'Lost connection',
+            'is dead or not enabled',
+            'Error while sending',
+            'decryption failed or bad record mac',
+            'server closed the connection unexpectedly',
+            'SSL connection has been closed unexpectedly',
+            'Error writing data to the connection',
+            'Resource deadlock avoided',
+            'failed with errno',
+        ];
+        $error = $e->getMessage();
+        foreach ($info as $msg) {
+            if (false !== stripos($error, $msg)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
